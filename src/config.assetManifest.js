1441691f2fea538ddd961855a25eac46e161a257
Alex Leitch <alex.leitch@gmail.com>
2017-12-03 19:48:25 -0500

    Update config to globals and use AssetManifest

---
+++ src/config.assetManifest.js
@@ -0,0 +1,25 @@
+const manifest = {
+  spritesheets: [
+  ],
+  bitmap_fonts: [],
+  audio: [],
+  images: [
+    'player',
+    'mushroom',
+    'loader-bg',
+    'couch',
+    'door',
+    'wall1',
+    'scifi_platformTiles_32x32'
+  ],
+  fonts: {
+    custom: {
+      families: []
+    },
+    google: {
+      families: [
+      ]
+    }
+  }
+};
+export default manifest;
---
+++ src/config.globals.js
@@ -0,0 +1,12 @@
+export default {
+  gameWidth: 800,
+  gameHeight: 600,
+  worldHeight: 30,
+  worldWidth: 50,
+  blockSize: 32,
+  collisionLayer: {},
+  map: {},
+  player: {},
+  cursors: {},
+  localStorageName: 'parisNightTrain'
+};
